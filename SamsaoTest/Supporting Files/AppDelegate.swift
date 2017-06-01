209947b91d32e10c8248a6d7ae7c050997eaeff6
Rajinder Ramgarhia <rramgarhia@samsao.co>
2017-06-01 12:58:34 -0400

    Update to Swift 3

--- SamsaoTest/Supporting Files/AppDelegate.swift
+++ SamsaoTest/Supporting Files/AppDelegate.swift
@@ -12,10 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window!.makeKeyAndVisible()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
 
         return true
     }
